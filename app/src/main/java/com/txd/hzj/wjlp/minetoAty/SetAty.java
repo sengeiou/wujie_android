b2286aac28629c5593ee74526893cdc1e9b4df34
lienchao <470360046@qq.com>
2017-07-13 18:08:31 +0800

    我的页面以及设置页更新

--- app/src/main/java/com/txd/hzj/wjlp/minetoAty/SetAty.java
+++ app/src/main/java/com/txd/hzj/wjlp/minetoAty/SetAty.java
@@ -3,6 +3,7 @@ package com.txd.hzj.wjlp.minetoAty;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
