701c8b34c912843248ce087709aa28ba891582aa
hzj_duke <1215745879@qq.com>
2017-09-11 14:30:43 +0800

    修改购物券界面

--- app/src/main/java/com/txd/hzj/wjlp/minetoAty/tricket/MyCouponAty.java
+++ app/src/main/java/com/txd/hzj/wjlp/minetoAty/tricket/MyCouponAty.java
@@ -9,8 +9,6 @@ import android.widget.TextView;
 import com.ants.theantsgo.tool.ToolKit;
 import com.ants.theantsgo.util.JSONUtils;
 import com.ants.theantsgo.view.inScroll.ListViewForScrollView;
-import com.ants.theantsgo.view.pulltorefresh.PullToRefreshBase;
-import com.ants.theantsgo.view.pulltorefresh.PullToRefreshScrollView;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
 import com.txd.hzj.wjlp.R;
@@ -43,24 +41,22 @@ public class MyCouponAty extends BaseAty {
     @ViewInject(R.id.un_valid_ticket_lv)
     private ListViewForScrollView un_valid_ticket_lv;
 
-    private TricketAdapter tricketAdapter;
-    private TricketAdapter tricketAdapter1;
-
     private int p = 1;
     /**
      * 分页数据
      */
     @ViewInject(R.id.pull_refresh_sc)
-    private PullToRefreshScrollView pull_refresh_sc;
+    private ScrollView pull_refresh_sc;
 
     private UserPst userPst;
 
     // 过期的
     private List<Map<String, String>> out;
-    private List<Map<String, String>> out2;
     // 能正常使用
     private List<Map<String, String>> normal;
-    private List<Map<String, String>> normal2;
+
+    public MyCouponAty() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,18 +66,6 @@ public class MyCouponAty extends BaseAty {
         titlt_right_tv.setText("明细");
         titlt_right_tv.setVisibility(View.VISIBLE);
         titlt_right_tv.setTextColor(ContextCompat.getColor(this, R.color.colorAccent));
-
-        pull_refresh_sc.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ScrollView>() {
-            @Override
-            public void onPullDownToRefresh(PullToRefreshBase<ScrollView> refreshView) {
-                p = 1;
-            }
-
-            @Override
-            public void onPullUpToRefresh(PullToRefreshBase<ScrollView> refreshView) {
-                p++;
-            }
-        });
     }
 
     @Override
@@ -106,10 +90,7 @@ public class MyCouponAty extends BaseAty {
     protected void initialized() {
         userPst = new UserPst(this);
         out = new ArrayList<>();
-        out2 = new ArrayList<>();
         normal = new ArrayList<>();
-        normal2 = new ArrayList<>();
-
     }
 
     @Override
@@ -123,30 +104,16 @@ public class MyCouponAty extends BaseAty {
         Map<String, String> map = JSONUtils.parseKeyAndValueToMap(jsonStr);
         if (requestUrl.contains("vouchersList")) {
             Map<String, String> data = JSONUtils.parseKeyAndValueToMap(map.get("data"));
-            if (1 == p) {
-                if (ToolKit.isList(data, "out")) {
-                    out = JSONUtils.parseKeyAndValueToMapList(data.get("out"));
-                    tricketAdapter1 = new TricketAdapter(1, this, out);
-                    un_valid_ticket_lv.setAdapter(tricketAdapter1);
-                }
-                if (ToolKit.isList(data, "normal")) {
-                    normal = JSONUtils.parseKeyAndValueToMapList(data.get("normal"));
-                    tricketAdapter = new TricketAdapter(0, this, normal);
-                    valid_ticket_lv.setAdapter(tricketAdapter);
-                }
-            } else {
-                if (ToolKit.isList(data, "out")) {
-                    out2 = JSONUtils.parseKeyAndValueToMapList(data.get("out"));
-                    out.addAll(out2);
-                    tricketAdapter1.notifyDataSetChanged();
-                }
-                if (ToolKit.isList(data, "normal")) {
-                    normal2 = JSONUtils.parseKeyAndValueToMapList(data.get("normal"));
-                    normal.addAll(normal2);
-                    tricketAdapter.notifyDataSetChanged();
-                }
+            if (ToolKit.isList(data, "out")) {
+                out = JSONUtils.parseKeyAndValueToMapList(data.get("out"));
+                TricketAdapter tricketAdapter1 = new TricketAdapter(1, this, out);
+                un_valid_ticket_lv.setAdapter(tricketAdapter1);
             }
-            pull_refresh_sc.onRefreshComplete();
+            if (ToolKit.isList(data, "normal")) {
+                normal = JSONUtils.parseKeyAndValueToMapList(data.get("normal"));
+                TricketAdapter tricketAdapter = new TricketAdapter(0, this, normal);
+                valid_ticket_lv.setAdapter(tricketAdapter);
+            }
         }
     }
 }
