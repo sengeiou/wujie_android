2f5faeb221b78517a007be4c0efd760e294c0656
Quyl <aliang_0223@163.com>
2018-05-22 10:43:41 +0800

    优化订单详情页面布局

--- app/src/main/java/com/txd/hzj/wjlp/minetoAty/order/CollageDetailsAty.java
+++ app/src/main/java/com/txd/hzj/wjlp/minetoAty/order/CollageDetailsAty.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.Space;
 import android.widget.TextView;
 
 import com.ants.theantsgo.config.Settings;
@@ -118,6 +119,7 @@ public class CollageDetailsAty extends BaseAty {
     private LinearLayout lin_logistics;
     private String type2WL = null;//给物流的type  0 普通商品 1 拼单购
     private boolean isTy = false;
+    private Space buttom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -158,6 +160,7 @@ public class CollageDetailsAty extends BaseAty {
     @Override
     protected void initialized() {
         thisAdapter = new ThisGoodsAdapter();
+        buttom = (Space) findViewById(R.id.pay_buttom);
     }
 
     @Override
@@ -346,42 +349,56 @@ public class CollageDetailsAty extends BaseAty {
     private void setGroupBuyOrderStatus() {
         switch (order_status) {
             case "0":
+                titlt_conter_tv.setText("待支付订单");
                 tv_state.setText("待支付");
                 tv_btn_left.setText("取消订单");
                 tv_btn_right.setText("付款");
                 layout_choose_address.setVisibility(View.GONE);
                 break;
             case "1":
+                titlt_conter_tv.setText("待成团订单");
                 tv_state.setText("待成团");
                 tv_btn_left.setVisibility(View.GONE);
                 tv_btn_right.setVisibility(View.GONE);
+                bot_for_order.setVisibility(View.GONE);
+                buttom.setVisibility(View.GONE);
                 break;
             case "2":
+                titlt_conter_tv.setText("待发货订单");
                 tv_state.setText("待发货");
                 tv_btn_left.setVisibility(View.GONE);
                 tv_btn_right.setVisibility(View.GONE);
+                bot_for_order.setVisibility(View.GONE);
+                buttom.setVisibility(View.GONE);
                 break;
             case "3":
+                titlt_conter_tv.setText("待收货订单");
                 tv_state.setText("待收货");
                 tv_btn_left.setVisibility(View.GONE);
                 tv_btn_right.setText("确认收货");
-                tv_btn_right.setVisibility(View.GONE);
+                tv_btn_right.setVisibility(View.VISIBLE);
                 break;
             case "4":
+                titlt_conter_tv.setText("待评价订单");
                 tv_state.setText("待评价");
                 tv_btn_left.setVisibility(View.GONE);
                 tv_btn_right.setText("评价");
+                bot_for_order.setVisibility(View.VISIBLE);
                 break;
             case "5":
+                titlt_conter_tv.setText("已完成订单");
                 tv_state.setText("已完成");
                 tv_btn_left.setVisibility(View.GONE);
                 tv_btn_right.setText("删除");
+                bot_for_order.setVisibility(View.VISIBLE);
                 break;
             case "6":
+                titlt_conter_tv.setText("已取消订单");
                 tv_state.setText("已取消");
                 tv_btn_left.setVisibility(View.GONE);
                 layout_choose_address.setVisibility(View.GONE);
                 tv_btn_right.setText("删除");
+                bot_for_order.setVisibility(View.VISIBLE);
                 break;
 
         }
@@ -461,7 +478,7 @@ public class CollageDetailsAty extends BaseAty {
                     tgvh.tv_btn_right.setVisibility(View.VISIBLE); // 中间按钮催发货显示
                 }
                 tgvh.tv_btn_remind.setVisibility(View.VISIBLE);
-            }else if (order_status.equals("3")) { // 订单待收货状态
+            } else if (order_status.equals("3")) { // 订单待收货状态
                 if (map.containsKey("sale_status")) {
                     tgvh.delayReceiving.setVisibility(map.get("sale_status").equals("0") ? View.VISIBLE : View.GONE); // 延长收货按钮显示
                 } else {
@@ -470,7 +487,7 @@ public class CollageDetailsAty extends BaseAty {
                 tgvh.tv_btn_right.setText("确认收货"); // 订单待收货状态下设置中间按钮为：确认收货
                 tgvh.tv_btn_left.setVisibility(View.VISIBLE);
                 tgvh.tv_btn_right.setVisibility(View.VISIBLE); // 中间的确认收货显示
-            }else if (order_status.equals("6")) { // 订单为已取消状态
+            } else if (order_status.equals("6")) { // 订单为已取消状态
                 tgvh.tv_btn_right.setVisibility(View.GONE); // 隐藏付款按钮
                 tgvh.tv_btn_left.setVisibility(View.GONE); // 申请售后按钮隐藏
                 tgvh.delayReceiving.setVisibility(View.GONE); // 延长收货隐藏
