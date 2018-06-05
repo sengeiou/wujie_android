800da74f356f0c15a3344eb4f9d972e2ad882517
zhangyunfei <32457127@qq.com>
2018-06-05 18:05:56 +0800

    将接口改为测试接口

--- frame/src/main/java/com/ants/theantsgo/config/Config.java
+++ frame/src/main/java/com/ants/theantsgo/config/Config.java
@@ -26,8 +26,8 @@ public class Config {
 
     private static View[] aty = new View[1];
 
-    public final static String BASE_URL = "http://api.wujiemall.com/index.php/Api/"; // 正式服务器
-//    public final static String BASE_URL = "http://test.wujiemall.com/index.php/Api/"; // 测试服务器
+//    public final static String BASE_URL = "http://api.wujiemall.com/index.php/Api/"; // 正式服务器
+    public final static String BASE_URL = "http://test.wujiemall.com/index.php/Api/"; // 测试服务器
 //    public final static String BASE_URL = "http://htest.wujiemall.com/index.php/Api/"; // 测试拼单服务器
 
     public static List<Activity> list = new ArrayList<>();
