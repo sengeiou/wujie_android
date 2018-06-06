e64ccc163d93085ef253f62441fee97e784df5d7
Quyl <aliang_0223@163.com>
2018-06-06 16:22:18 +0800

    添加分销相关，订单管理页面。优化电铺管理首页

--- app/src/main/java/com/txd/hzj/wjlp/distribution/shopmanage/ShopMain.java
+++ app/src/main/java/com/txd/hzj/wjlp/distribution/shopmanage/ShopMain.java
@@ -23,6 +23,8 @@ public class ShopMain extends BaseAty implements OnClickListener {
     private LinearLayout shopSetUp;
     private LinearLayout shopPersonManage;
     private LinearLayout shopOrderManage;
+    private TextView visitor;
+    private TextView orderNum;
 
     @Override
     protected int getLayoutResId() {
@@ -44,7 +46,10 @@ public class ShopMain extends BaseAty implements OnClickListener {
         shopPersonManage = findViewById(R.id.shop_person_manage);
         //订单管理
         shopOrderManage = findViewById(R.id.shop_order_magage);
-
+        //访客量
+        visitor = findViewById(R.id.shop_visitor);
+        //订单量
+        orderNum = findViewById(R.id.shop_order);
 
         //注册点击事件
         shopUpGoods.setOnClickListener(this);
@@ -57,14 +62,15 @@ public class ShopMain extends BaseAty implements OnClickListener {
 
     @Override
     protected void requestData() {
-    titleName.setText("店铺管理");
-
+        titleName.setText("店铺管理");
+        visitor.setText("访客量" + "\n100");
+        orderNum.setText("订单量" + "\n7");
     }
 
     @Override
     public void onClick(View v) {
         super.onClick(v);
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.shop_up_goods:
                 break;
             case R.id.shop_goods_manage:
@@ -73,14 +79,15 @@ public class ShopMain extends BaseAty implements OnClickListener {
                 break;
             case R.id.shop_set_up:
                 //店铺设置
-                startActivity(ShopSetUp.class,null);
+                startActivity(ShopSetUp.class, null);
                 break;
             case R.id.shop_person_manage:
                 //顾客管理
-                startActivity(ShopPersonManage.class,null);
+                startActivity(ShopPersonManage.class, null);
                 break;
             case R.id.shop_order_magage:
                 //订单管理
+                startActivity(ShopOrderManage.class, null);
                 break;
         }
     }
---
+++ app/src/main/java/com/txd/hzj/wjlp/distribution/shopmanage/ShopOrderManage.java
@@ -0,0 +1,42 @@
+package com.txd.hzj.wjlp.distribution.shopmanage;
+
+import android.support.design.widget.TabLayout;
+import android.view.View;
+import android.widget.TextView;
+
+import com.txd.hzj.wjlp.R;
+import com.txd.hzj.wjlp.base.BaseAty;
+
+/**
+ * 创建者：Qyl
+ * 创建时间：2018/6/6 0006 15:14
+ * 功能描述：
+ * 联系方式：无
+ */
+public class ShopOrderManage extends BaseAty {
+
+    private TabLayout tabView;
+    private TextView titleName;
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.shop_order_manage;
+    }
+
+    @Override
+    protected void initialized() {
+        tabView = findViewById(R.id.shop_tab_view);
+        titleName = findViewById(R.id.titlt_conter_tv);
+
+    }
+
+    @Override
+    protected void requestData() {
+        titleName.setText("订单管理");
+        tabView.addTab(tabView.newTab().setText("全部"));
+        tabView.addTab(tabView.newTab().setText("代付款"));
+        tabView.addTab(tabView.newTab().setText("代发货"));
+        tabView.addTab(tabView.newTab().setText("待收货"));
+        tabView.addTab(tabView.newTab().setText("已完成"));
+    }
+}
