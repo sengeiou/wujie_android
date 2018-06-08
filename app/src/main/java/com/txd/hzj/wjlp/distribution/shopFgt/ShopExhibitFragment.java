2da5fb3c635459c53dc404634c47f8770fa76dbb
zhangyunfei <32457127@qq.com>
2018-06-08 12:00:49 +0800

    小店上货界面搭建

--- app/src/main/java/com/txd/hzj/wjlp/distribution/shopFgt/ShopExhibitFragment.java
+++ app/src/main/java/com/txd/hzj/wjlp/distribution/shopFgt/ShopExhibitFragment.java
@@ -1,7 +1,13 @@
 package com.txd.hzj.wjlp.distribution.shopFgt;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
 import com.txd.hzj.wjlp.R;
 import com.txd.hzj.wjlp.base.BaseFgt;
 
@@ -15,10 +21,24 @@ public class ShopExhibitFragment extends BaseFgt {
     private String mParam1;
     private String mParam2;
 
+    //积分
+    @ViewInject(R.id.internal_tv)
+    TextView internal_tv;
+    //代金券
+    @ViewInject(R.id.cash_coupon_tv)
+    TextView cash_coupon_tv;
+    //销量
+    @ViewInject(R.id.sales_volume_tv)
+    TextView sales_volume_tv;
+    //价格
+    @ViewInject(R.id.price_tv)
+    TextView price_tv;
 
-    public ShopExhibitFragment() {
-        // Required empty public constructor
-    }
+    private String redColor="#ffe71f19";
+    private String blgColor="#ff333333";
+    private Drawable selectId;
+    private Drawable unSelectId;
+
 
     public static ShopExhibitFragment newInstance(String param1, String param2) {
         ShopExhibitFragment fragment = new ShopExhibitFragment();
@@ -46,7 +66,10 @@ public class ShopExhibitFragment extends BaseFgt {
 
     @Override
     protected void initialized() {
-
+        selectId = getResources().getDrawable(R.drawable.shopjiantou);
+        unSelectId = getResources().getDrawable(R.drawable.shopblgjiantou);
+        selectId.setBounds(0,0,selectId.getMinimumWidth(),selectId.getMinimumHeight());
+        unSelectId.setBounds(0,0,unSelectId.getMinimumWidth(),unSelectId.getMinimumHeight());
     }
 
     @Override
@@ -59,4 +82,53 @@ public class ShopExhibitFragment extends BaseFgt {
     protected void immersionInit() {
 
     }
+
+    @Override
+    @OnClick({R.id.internal_tv,R.id.cash_coupon_tv,R.id.sales_volume_tv,R.id.price_tv})
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()){
+            case R.id.internal_tv:
+                internal_tv.setTextColor(Color.parseColor(redColor));
+                internal_tv.setCompoundDrawables(null,null,selectId,null);
+                cash_coupon_tv.setTextColor(Color.parseColor(blgColor));
+                cash_coupon_tv.setCompoundDrawables(null,null,unSelectId,null);
+                sales_volume_tv.setTextColor(Color.parseColor(blgColor));
+                sales_volume_tv.setCompoundDrawables(null,null,unSelectId,null);
+                price_tv.setTextColor(Color.parseColor(blgColor));
+                price_tv.setCompoundDrawables(null,null,unSelectId,null);
+                break;
+            case R.id.cash_coupon_tv:
+                internal_tv.setTextColor(Color.parseColor(blgColor));
+                internal_tv.setCompoundDrawables(null,null,unSelectId,null);
+                cash_coupon_tv.setTextColor(Color.parseColor(redColor));
+                cash_coupon_tv.setCompoundDrawables(null,null,selectId,null);
+                sales_volume_tv.setTextColor(Color.parseColor(blgColor));
+                sales_volume_tv.setCompoundDrawables(null,null,unSelectId,null);
+                price_tv.setTextColor(Color.parseColor(blgColor));
+                price_tv.setCompoundDrawables(null,null,unSelectId,null);
+                break;
+            case R.id.sales_volume_tv:
+                internal_tv.setTextColor(Color.parseColor(blgColor));
+                internal_tv.setCompoundDrawables(null,null,unSelectId,null);
+                cash_coupon_tv.setTextColor(Color.parseColor(blgColor));
+                cash_coupon_tv.setCompoundDrawables(null,null,unSelectId,null);
+                sales_volume_tv.setTextColor(Color.parseColor(redColor));
+                sales_volume_tv.setCompoundDrawables(null,null,selectId,null);
+                price_tv.setTextColor(Color.parseColor(blgColor));
+                price_tv.setCompoundDrawables(null,null,unSelectId,null);
+                break;
+            case R.id.price_tv:
+                internal_tv.setTextColor(Color.parseColor(blgColor));
+                internal_tv.setCompoundDrawables(null,null,unSelectId,null);
+                cash_coupon_tv.setTextColor(Color.parseColor(blgColor));
+                cash_coupon_tv.setCompoundDrawables(null,null,unSelectId,null);
+                sales_volume_tv.setTextColor(Color.parseColor(blgColor));
+                sales_volume_tv.setCompoundDrawables(null,null,unSelectId,null);
+                price_tv.setTextColor(Color.parseColor(redColor));
+                price_tv.setCompoundDrawables(null,null,selectId,null);
+                break;
+        }
+    }
+
 }
