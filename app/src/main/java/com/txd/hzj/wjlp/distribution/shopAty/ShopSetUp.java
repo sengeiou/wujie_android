38024a29e72dd2d14647290b08ea50cd670e6d30
Quyl <aliang_0223@163.com>
2018-06-14 11:44:53 +0800

    添加店铺设置拍照以及相册选择照片功能

--- app/src/main/java/com/txd/hzj/wjlp/distribution/shopAty/ShopSetUp.java
+++ app/src/main/java/com/txd/hzj/wjlp/distribution/shopAty/ShopSetUp.java
@@ -10,13 +10,13 @@ import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.ants.theantsgo.imageLoader.GlideImageLoader;
 import com.ants.theantsgo.util.CompressionUtil;
 import com.ants.theantsgo.util.L;
 import com.bumptech.glide.Glide;
 import com.lzy.imagepicker.ImagePicker;
 import com.lzy.imagepicker.bean.ImageItem;
 import com.lzy.imagepicker.ui.ImageGridActivity;
-import com.lzy.imagepicker.ui.ImagePreviewActivity;
 import com.txd.hzj.wjlp.R;
 import com.txd.hzj.wjlp.base.BaseAty;
 
@@ -43,6 +43,7 @@ public class ShopSetUp extends BaseAty implements View.OnClickListener {
     private File file1;
     private ShapedImageView shopImage;
 
+
     @Override
     protected int getLayoutResId() {
         return R.layout.shop_set_up;
@@ -50,19 +51,36 @@ public class ShopSetUp extends BaseAty implements View.OnClickListener {
 
     @Override
     protected void initialized() {
+
         setIma = findViewById(R.id.shop_set_ima);
         shopImage = findViewById(R.id.img_head);
         titleName = findViewById(R.id.titlt_conter_tv);
-        shop_person_title_manage=findViewById(R.id.shop_person_title_manage);
+        shop_person_title_manage = findViewById(R.id.shop_person_title_manage);
         shop_person_title_manage.setVisibility(View.GONE);
         titleName.setText("店铺设置");
         //注册点击事件
         setIma.setOnClickListener(this);
+        forImagePacker();
     }
 
     @Override
     protected void requestData() {
 
+    }
+/**
+ * 设置拍照数据
+ * */
+    private void forImagePacker() {
+        ImagePicker imagePicker = ImagePicker.getInstance();
+        imagePicker.setImageLoader(new GlideImageLoader());// 图片加载
+        imagePicker.setCrop(true);// 裁剪
+        imagePicker.setSaveRectangle(true);// 矩形保存
+        imagePicker.setFocusWidth(500);//裁剪框宽度
+        imagePicker.setFocusHeight(500);// 裁剪框高度
+        imagePicker.setOutPutX(1000);// 保存图片高度
+        imagePicker.setOutPutY(1000);// 保存图片宽度
+        imagePicker.setMultiMode(false);// 但须
+        imagePicker.setShowCamera(true);// 显示拍照按钮
     }
 
     @Override
@@ -79,19 +97,19 @@ public class ShopSetUp extends BaseAty implements View.OnClickListener {
     private void showDialogs() {
 
         view = View.inflate(ShopSetUp.this, R.layout.shop_dialog_popup, null);
-         view.findViewById(R.id.shop_dialog_disimis).setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 popupWindow.dismiss();
-             }
-         });
+        view.findViewById(R.id.shop_dialog_disimis).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
 
         //跟布局
         RelativeLayout view1 = (RelativeLayout) view.findViewById(R.id.shop_set_main);
         view1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (popupWindow != null && popupWindow.isShowing()){
+                if (popupWindow != null && popupWindow.isShowing()) {
                     popupWindow.dismiss();
                     popupWindow = null;
                 }
@@ -105,14 +123,16 @@ public class ShopSetUp extends BaseAty implements View.OnClickListener {
         setPicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(ShopSetUp.this, ImagePreviewActivity.class);
-                startActivityForResult(i, 1001);
+                Intent intent = new Intent(ShopSetUp.this, ImageGridActivity.class);
+                intent.putExtra(ImageGridActivity.EXTRAS_TAKE_PICKERS, true); // 是否是直接打开相机
+                startActivityForResult(intent, 100);
             }
         });
         setAlbum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent in = new Intent(ShopSetUp.this, ImageGridActivity.class);
+                startActivityForResult(in, 102);
             }
         });
         popupWindow = new PopupWindow(view, RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
@@ -121,7 +141,7 @@ public class ShopSetUp extends BaseAty implements View.OnClickListener {
         popupWindow.setOutsideTouchable(false);
         popupWindow.setTouchable(true);
         popupWindow.setFocusable(false);
-        popupWindow.showAtLocation(view1, Gravity.BOTTOM , 0,
+        popupWindow.showAtLocation(view1, Gravity.BOTTOM, 0,
                 0);
         popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
             @Override
@@ -133,29 +153,29 @@ public class ShopSetUp extends BaseAty implements View.OnClickListener {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == ImagePicker.RESULT_CODE_ITEMS) {
-            try {
-                if (data != null && data.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS) != null) {
-                    ArrayList<ImageItem> images = (ArrayList<ImageItem>) data.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS);
-                    String pic_path = CompressionUtil.compressionBitmap(images.get(0).path);
-                    switch (requestCode) {
-                        case 101: // 身份证正面照
-                            if (!pic_path.equals("")) {
-                                file1 = new File(pic_path);
-                                if (file1 != null && file1.isFile()) {
-                                    Glide.with(this).load(file1).error(R.mipmap.icon_idcard_front)
-                                            .placeholder(R.mipmap.icon_idcard_front).centerCrop().into(shopImage);
-                                }
-                            }
-                            break;
+
+        try {
+            if (data != null && data.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS) != null) {
+                ArrayList<ImageItem> images = (ArrayList<ImageItem>) data.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS);
+                String pic_path = CompressionUtil.compressionBitmap(images.get(0).path);
+
+                // 设置照片
+                if (!pic_path.equals("")) {
+                    //图片路径
+                    file1 = new File(pic_path);
+                    if (file1 != null && file1.isFile()) {
+                        Glide.with(ShopSetUp.this).load(file1).error(R.mipmap.icon_idcard_front)
+                                .placeholder(R.mipmap.icon_idcard_front).centerCrop().into(shopImage);
                     }
+
                 }
-            } catch (Exception e) {
-                L.e("fragment1 onActivityResult is exception:" + e.toString());
-                showErrorTip("图片选择出现异常");
             }
+            popupWindow.dismiss();
+        } catch (Exception e) {
+            L.e("fragment1 onActivityResult is exception:" + e.toString());
+            popupWindow.dismiss();
         }
     }
 
@@ -167,6 +187,7 @@ public class ShopSetUp extends BaseAty implements View.OnClickListener {
         }
         return super.onTouchEvent(event);
     }
+
     public void setBackgroundAlpha(float bgAlpha) {
         WindowManager.LayoutParams lp = (this).getWindow()
                 .getAttributes();
