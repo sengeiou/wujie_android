ad5b535e3db6958bb6f693b028211262957f3c6c
wjj <974282726@qq.com>
2018-07-10 17:37:20 +0800

    1、大致的解决了一下Android8.0+的环信启动报错问题

--- app/src/main/java/com/txd/hzj/wjlp/huanxin/receiver/StartServiceReceiver.java
+++ app/src/main/java/com/txd/hzj/wjlp/huanxin/receiver/StartServiceReceiver.java
@@ -14,6 +14,7 @@ package com.txd.hzj.wjlp.huanxin.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.widget.Toast;
 
 import com.ants.theantsgo.tips.ToastTip;
@@ -37,7 +38,14 @@ public class StartServiceReceiver extends BroadcastReceiver {
         try {
             Intent startServiceIntent = new Intent(context, EMChatService.class);
             startServiceIntent.putExtra("reason", "boot");
-            context.startService(startServiceIntent);
+//            context.startService(startServiceIntent); // 此方式在Android_8.0+会报IllegalStateException异常
+            // 参考解决地址：https://blog.csdn.net/u010784887/article/details/79675147
+            // 遗留问题：未service里再调用startForeground方法
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(startServiceIntent);
+            } else {
+                context.startService(startServiceIntent);
+            }
         } catch (IllegalStateException e) {
             L.e("huanxin.receiver.StartServiceReceiver:" + e.toString());
             Toast.makeText(DemoApplication.applicationContext, "环信服务启动异常", Toast.LENGTH_SHORT);
