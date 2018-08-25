6ca2210632ec4c3ee1ad08d7808dfd318513772c
zhangyunfei <32457127@qq.com>
2018-08-25 10:07:50 +0800

    线下店铺定位功能

--- app/src/main/java/com/txd/hzj/wjlp/citySelect/MellCitySelectAty.java
+++ app/src/main/java/com/txd/hzj/wjlp/citySelect/MellCitySelectAty.java
@@ -1,19 +1,26 @@
 package com.txd.hzj.wjlp.citySelect;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.lidroid.xutils.ViewUtils;
@@ -67,6 +74,9 @@ public class MellCitySelectAty extends BaseAty {
     @ViewInject(R.id.select_city_layout)
     private FrameLayout select_city_layout;
 
+    @ViewInject(R.id.search_edit)
+    private EditText search_edit;
+
     /**
      * 城市搜索按钮
      */
@@ -82,11 +92,34 @@ public class MellCitySelectAty extends BaseAty {
     private boolean selectCity = true;
 
 
+    /**
+     * 定位城市
+     */
+    @ViewInject(R.id.city_tv1)
+    private TextView city_tv1;
+
+    @ViewInject(R.id.city_tv2)
+    private TextView city_tv2;
+
+    /**
+     * 重新定位
+     */
+    @ViewInject(R.id.reLocation_tv)
+    private TextView reLocation_tv;
+
+
     @ViewInject(R.id.get_poi_search_lv)
     private ListView get_poi_search_lv;
 
     private POIAdapter poiAdapter;
     private List<City> list;
+    private LocationClient mLocationClient;
+    private MyLocationListener myListener = new MyLocationListener();
+    private List<Poi> mPoiList;
+
+    /**
+     * poi
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -147,11 +180,11 @@ public class MellCitySelectAty extends BaseAty {
                 }
             }
         });
-        get_poi_search_lv.setAdapter(poiAdapter);
+
     }
 
     @Override
-    @OnClick({R.id.switch_search_or_select_layout})
+    @OnClick({R.id.switch_search_or_select_layout,R.id.search_for_city_tv,R.id.reLocation_tv})
     public void onClick(View v) {
         super.onClick(v);
         switch (v.getId()) {
@@ -181,6 +214,43 @@ public class MellCitySelectAty extends BaseAty {
                 }
 
                 break;
+            case R.id.search_for_city_tv:
+                String searchContent = search_edit.getText().toString().trim();
+                if (TextUtils.isEmpty(searchContent)){
+                    showToast("搜索内容不能为空");
+                    return;
+                }
+                for (int i = 0; i < mPoiList.size(); i++) {
+                    if (mPoiList.get(i).getName().contains(searchContent)){
+                        get_poi_search_lv.smoothScrollToPosition(i);
+                        break;
+                    }
+                }
+                break;
+
+            case R.id.reLocation_tv:
+                if (mLocationClient!=null && !mLocationClient.isStarted()){
+                    LocationClientOption option = new LocationClientOption();
+
+                    option.setIsNeedAddress(true);
+                    //可选，是否需要地址信息，默认为不需要，即参数为false
+                    //如果开发者需要获得当前点的地址信息，此处必须为true
+
+                    option.setIsNeedLocationDescribe(true);
+                    //可选，是否需要位置描述信息，默认为不需要，即参数为false
+                    //如果开发者需要获得当前点的位置信息，此处必须为true
+
+                    option.setIsNeedLocationPoiList(true);
+                    //可选，是否需要周边POI信息，默认为不需要，即参数为false
+                    //如果开发者需要获得周边POI信息，此处必须为true
+
+                    mLocationClient.setLocOption(option);
+                    //mLocationClient为第二步初始化过的LocationClient对象
+                    //需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+                    //更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
+                    mLocationClient.start();
+                }
+                break;
         }
     }
 
@@ -191,7 +261,29 @@ public class MellCitySelectAty extends BaseAty {
 
     @Override
     protected void initialized() {
-        poiAdapter = new POIAdapter();
+
+        mLocationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);
+        //注册监听函数
+        LocationClientOption option = new LocationClientOption();
+
+        option.setIsNeedAddress(true);
+        //可选，是否需要地址信息，默认为不需要，即参数为false
+        //如果开发者需要获得当前点的地址信息，此处必须为true
+
+        option.setIsNeedLocationDescribe(true);
+        //可选，是否需要位置描述信息，默认为不需要，即参数为false
+        //如果开发者需要获得当前点的位置信息，此处必须为true
+
+        option.setIsNeedLocationPoiList(true);
+        //可选，是否需要周边POI信息，默认为不需要，即参数为false
+        //如果开发者需要获得周边POI信息，此处必须为true
+
+        mLocationClient.setLocOption(option);
+        //mLocationClient为第二步初始化过的LocationClient对象
+        //需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+        //更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
     }
 
     @Override
@@ -199,18 +291,39 @@ public class MellCitySelectAty extends BaseAty {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mLocationClient!=null){
+            mLocationClient.start();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mLocationClient!=null){
+            mLocationClient.stop();
+        }
+    }
+
     private class POIAdapter extends BaseAdapter {
 
         private PoiVh poiVh;
+        private List<Poi> mPoiList;
+
+        public POIAdapter(List<Poi> poiList) {
+            mPoiList = poiList;
+        }
 
         @Override
         public int getCount() {
-            return 10;
+            return mPoiList.size()>0?mPoiList.size():0;
         }
 
         @Override
         public Object getItem(int i) {
-            return null;
+            return mPoiList.get(i);
         }
 
         @Override
@@ -229,13 +342,42 @@ public class MellCitySelectAty extends BaseAty {
             } else {
                 poiVh = (PoiVh) view.getTag();
             }
-            poiVh.text_context_tv.setText("鑫茂科技园");
+            poiVh.text_context_tv.setText(mPoiList.get(i).getName());
             return view;
         }
 
         private class PoiVh {
             @ViewInject(R.id.text_context_tv)
             private TextView text_context_tv;
+        }
+    }
+
+
+    private class MyLocationListener extends BDAbstractLocationListener{
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+            //以下只列举部分获取地址相关的结果信息
+            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+
+            String addr = location.getAddrStr();    //获取详细地址信息
+            String country = location.getCountry();    //获取国家
+            String province = location.getProvince();    //获取省份
+            String city = location.getCity();    //获取城市
+            String district = location.getDistrict();    //获取区县
+            String street = location.getStreet();    //获取街道信息
+            String locationDescribe = location.getLocationDescribe();    //获取位置描述信息
+            //获取周边POI信息  POI信息包括POI ID、名称等，具体信息请参照类参考中POI类的相关说明
+            mPoiList = location.getPoiList();
+            city_tv1.setText(city);
+            city_tv2.setText(city);
+            if (null!=mPoiList){
+                poiAdapter = new POIAdapter(mPoiList);
+                get_poi_search_lv.setAdapter(poiAdapter);
+            }
+
+            Log.e("TAG", "address:"+addr+"\ncountry"+country +"\nprovince"+province+"\ncity"+city+"\ndistrict"+district+"\nstreet"+street+"\nlocationDescribe"+locationDescribe);
         }
     }
 
