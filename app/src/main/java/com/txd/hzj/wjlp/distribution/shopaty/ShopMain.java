1e075a04dcf79b2063f292b269c68a81e1940fa5
zhangyunfei <32457127@qq.com>
2018-11-21 14:55:45 +0800

    分销模块请求位置修改

--- app/src/main/java/com/txd/hzj/wjlp/distribution/shopaty/ShopMain.java
+++ app/src/main/java/com/txd/hzj/wjlp/distribution/shopaty/ShopMain.java
@@ -131,6 +131,11 @@ public class ShopMain extends BaseAty implements OnClickListener {
     @Override
     protected void requestData() {
         titleName.setText("链客网店");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         if (null != mExhibitPst && !TextUtils.isEmpty(mShop_id)) {
             mExhibitPst.shops(mShop_id);
         }
