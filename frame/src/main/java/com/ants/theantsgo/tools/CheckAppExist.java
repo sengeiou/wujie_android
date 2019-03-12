137e8b32bf9e04b87ee079282b747209beb644e6
zhangyunfei <32457127@qq.com>
2019-03-12 12:01:43 +0800

    微信判断

--- frame/src/main/java/com/ants/theantsgo/tools/CheckAppExist.java
+++ frame/src/main/java/com/ants/theantsgo/tools/CheckAppExist.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
 import java.util.List;
 /**
  * 作者：DUKE_HwangZj
@@ -52,11 +55,31 @@ public class CheckAppExist {
         if (pinfo != null) {
             for (PackageInfo info : pinfo) {
                 String pn = info.packageName;
-                if (pn.equals(packageName)) {
+                if (pn.equalsIgnoreCase(packageName)) {
                     return true;
                 }
             }
         }
         return false;
+    }
+
+
+    public  boolean isWeChatAppInstalled(Context context) {
+        IWXAPI api = WXAPIFactory.createWXAPI(context, "wxc971edce6f70ca57");
+        if(api.isWXAppInstalled() && api.isWXAppSupportAPI()) {
+            return true;
+        } else {
+            final PackageManager packageManager = context.getPackageManager();// 获取packagemanager
+            List<PackageInfo> pinfo = packageManager.getInstalledPackages(0);// 获取所有已安装程序的包信息
+            if (pinfo != null) {
+                for (int i = 0; i < pinfo.size(); i++) {
+                    String pn = pinfo.get(i).packageName;
+                    if (pn.equalsIgnoreCase("com.tencent.mm")) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
